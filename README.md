d9c04095612c90f0d6ea2e64b99cae4186d92fd8
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-08-28 18:30:07 +0530

    sync

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Mylo : Omnichain Token Bound Accounts
+
+## MyloWalletNFT
+
+0x9edb204f176f7182d6a6c3d4fd2381267ca8c97d
--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -10,7 +10,7 @@ import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
 const config = getDefaultConfig({
   appName: "Mylo",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-  chains: [arbitrum, arbitrumSepolia],
+  chains: [arbitrumSepolia, baseSepolia],
   // transports: {
   //   [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/...'),
   //   [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/...'),
