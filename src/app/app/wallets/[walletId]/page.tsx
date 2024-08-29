4d6537432429718550507266785b6fcef4ac3be0
0xyshv <writetoyshv@gmail.com>
2024-08-29 11:53:01 +0530

    chore: add error handling to transfer ETh

--- src/app/app/wallets/[walletId]/page.tsx
+++ src/app/app/wallets/[walletId]/page.tsx
@@ -32,6 +32,7 @@ import {
 } from "@/components/ui/tooltip";
 import { useToast } from "@/components/ui/use-toast";
 import { Skeleton } from "@/components/ui/skeleton";
+import Spinner from "@/components/Spinner";
 
 interface FormData {
   recipientAddress: string;
@@ -46,6 +47,7 @@ const WalletOverview = () => {
   const [nftAccount, setNftAccount] = useState<any>();
   const [isAccountActive, setIsAccountActive] = useState(false);
   const [isCreating, setIsCreating] = useState(false);
+  const [isTransferring, setIsTransferring] = useState(false);
   const { address, chainId, chain } = useAccount();
   const signer = useEthersSigner({ chainId: chainId });
   const [formData, setFormData] = useState<FormData>({
@@ -123,20 +125,39 @@ const WalletOverview = () => {
     }
   }, [tokenboundClient]);
 
-  // 🟡
   const transferETH = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!tokenboundClient || !address) return;
-    console.log(formData);
-    const executedTransfer = await tokenboundClient.transferETH({
-      account: nftAccount,
-      recipientAddress: formData.recipientAddress as `0x${string}`,
-      amount: Number(formData.amount),
-    });
-    // executedTransfer &&
-    //   alert(
-    //     `Sent ${formData.amount} ETH to ${formData.recipientAddress}`
-    //   );
+    if (!tokenboundClient || !address || !nftAccount) return;
+
+    setIsTransferring(true);
+    try {
+      const executedTransfer = await tokenboundClient.transferETH({
+        account: nftAccount,
+        recipientAddress: formData.recipientAddress as `0x${string}`,
+        amount: Number(formData.amount),
+      });
+
+      toast({
+        title: "Transfer Successful",
+        description: `Sent ${formData.amount} ETH to ${formData.recipientAddress}`,
+        duration: 3000,
+        variant: "success",
+      });
+    } catch (error) {
+      console.error("Transfer failed", error);
+      toast({
+        title: "Transfer Failed",
+        description: "There was an issue with the transaction. Please try again.",
+        duration: 3000,
+        variant: "destructive",
+      });
+    } finally {
+      setIsTransferring(false);
+      setFormData({
+        recipientAddress: "",
+        amount: 0,
+      });
+    }
   };
 
   useEffect(() => {
@@ -254,6 +275,7 @@ const WalletOverview = () => {
         {isAccountActive ? (
           <div className="flex gap-8 items-center justify-center">
             <Button>Connect Wallet to Dapp</Button>
+            {/* transfer ETH */}
             <Dialog>
               <DialogTrigger asChild>
                 <Button onClick={handleResetForm}>Transfer ETH</Button>
@@ -266,49 +288,59 @@ const WalletOverview = () => {
                   </DialogDescription>
                 </DialogHeader>
                 <form onSubmit={transferETH}>
-                  <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="recipientAddress" className="text-right">
-                        Recipient Address
-                      </Label>
-                      <Input
-                        id="recipientAddress"
-                        type="text"
-                        placeholder="0x..."
-                        className="col-span-3"
-                        value={formData.recipientAddress}
-                        onChange={(e: { target: { value: any } }) =>
-                          setFormData({
-                            ...formData,
-                            recipientAddress: e.target.value,
-                          })
-                        }
-                        required
-                      />
+                  {!isTransferring && (
+                    <>
+                      <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                          <Label htmlFor="recipientAddress" className="text-right">
+                            Recipient Address
+                          </Label>
+                          <Input
+                            id="recipientAddress"
+                            type="text"
+                            placeholder="0x..."
+                            className="col-span-3"
+                            value={formData.recipientAddress}
+                            onChange={(e: { target: { value: any } }) =>
+                              setFormData({
+                                ...formData,
+                                recipientAddress: e.target.value,
+                              })
+                            }
+                            required
+                          />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                          <Label htmlFor="amount" className="text-right">
+                            Amount
+                          </Label>
+                          <Input
+                            id="amount"
+                            placeholder="0.00"
+                            className="col-span-3"
+                            type="number"
+                            value={formData.amount}
+                            onChange={(e: { target: { value: any } }) =>
+                              setFormData({
+                                ...formData,
+                                amount: e.target.value,
+                              })
+                            }
+                            required
+                          />
+                        </div>
+                      </div>
+                      <Button type="submit" className="w-full">
+                        Transfer ETH
+                      </Button>
+                    </>
+                  )}
+                  {isTransferring && (
+                    <div className=" flex flex-col gap-3 items-center justify-center mt-4 text-center ">
+                      <Spinner />
+                      <p className="text-sm font-semibold text-muted-foreground">Transfer in progress....</p>
                     </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="amount" className="text-right">
-                        Amount
-                      </Label>
-                      <Input
-                        id="amount"
-                        placeholder="0.00"
-                        className="col-span-3"
-                        type="number"
-                        value={formData.amount}
-                        onChange={(e: { target: { value: any } }) =>
-                          setFormData({
-                            ...formData,
-                            amount: e.target.value,
-                          })
-                        }
-                        required
-                      />
-                    </div>
-                  </div>
-                  <Button type="submit" className="w-full">
-                    Transfer ETH
-                  </Button>
+                  )}
                 </form>
               </DialogContent>
             </Dialog>
